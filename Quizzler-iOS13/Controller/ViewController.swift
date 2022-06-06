b1955854d3f77432fd5ddd31d0c2994e480b6bd0
Joel <joellacerdaol@gmail.com>
2022-06-06 20:11:01 -0300

    final version

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+       
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        buttonA.setTitle(quizBrain.getAnswer()[0], for: .normal)
+        buttonB.setTitle(quizBrain.getAnswer()[1], for: .normal)
+        buttonC.setTitle(quizBrain.getAnswer()[2], for: .normal)
+        buttonA.backgroundColor = UIColor.clear
+        buttonB.backgroundColor = UIColor.clear
+        buttonC.backgroundColor = UIColor.clear
+        
+    }
+    
+}
+
--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -10,10 +10,12 @@ import Foundation
 
 struct Question {
     let text: String
-    let answer: String
+    let answers: [String]
+    let rightAnswer: String
     
-    init(q: String, a: String) {
-       text = q
-        answer = a
+    init(q: String, a: [String], correctAnswer: String) {
+        text = q
+        answers = a
+        rightAnswer = correctAnswer
     }
 }
